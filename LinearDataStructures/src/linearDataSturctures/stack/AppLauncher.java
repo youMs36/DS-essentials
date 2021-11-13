e9a12e7915fd3d1560d82ffd75f83d765c7061e8
youMs <urmad129@gmail.com>
2021-11-13 18:09:24 +0530

    firewall setting changed

--- LinearDataStructures/src/linearDataSturctures/stack/AppLauncher.java
+++ LinearDataStructures/src/linearDataSturctures/stack/AppLauncher.java
@@ -5,6 +5,7 @@ public class AppLauncher {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("THis will be pushed from eclipse");
+		System.out.println("after firewall update");
 
 	}
 
