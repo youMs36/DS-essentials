32da65e3b71b04415ee2ce2067a0c25aa9a80895
youMs <urmad129@gmail.com>
2021-11-13 18:10:52 +0530

    agiain

--- LinearDataStructures/src/linearDataSturctures/stack/AppLauncher.java
+++ LinearDataStructures/src/linearDataSturctures/stack/AppLauncher.java
@@ -5,7 +5,7 @@ public class AppLauncher {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("THis will be pushed from eclipse");
-		System.out.println("after firewall update");
+		System.out.println("after firewall update...");
 
 	}
 
